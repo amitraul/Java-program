c3333e2c870ded14708e8c692e92d62ee3631717
amit <amitaraula1234@gmail.com>
2022-12-25 10:30:15 +0530

    git commit -m "using of inheritance concept"

--- Amit.java
+++ Amit.java
@@ -1,14 +1,36 @@
-
-public class Amit{ 
-    public static void main(String[] args) {
-        for( int i=0;i<=15;i++){
-            if(i%2==0){
-                System.out.println("these no are even no:"+i);
-            }
-            else
-            System.out.println("these no are odd number:"+i);
-        }
-        System.out.println("the number was invalid");
-        
+    import java.util.*; 
+ class Animal {
+    String name;
+    String category;
+    public Animal(String name,String category){
+        this.name=name;
+        this.category=category;
+    }
+    public void printdetails(){
+        System.out.println("the name of animal:"+this.name +"\n their category:"+this.category);
     }
 }
+ class Bird{
+    String name;
+    String category;
+    int numberofwing;
+    public Bird(String name,String category,int numberofwing){
+        this.name=name;
+        this.category=category;
+        this.numberofwing=numberofwing;
+
+    }
+    public void printdetails(){
+        System.out.println("the name of bird:"+this.name +"\n the name of category:"+this.category +"\n number of wing:"+this.numberofwing);
+
+    } 
+}
+public class Amit{
+    public static void main(String[]args){
+        Animal b1=new Animal("tiger","mamaal");
+        Bird b2=new Bird("eagle","aves",2);
+        b1.printdetails();
+       b2.printdetails();
+    }
+}
+    
